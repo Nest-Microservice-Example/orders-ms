1651c5d7273d39b82626a3fa0e46000e5eccb1b1
Caleb Isaac Mora Diaz <calexico659@gmail.com>
2024-07-19 11:27:29 -0500

    feat: se levanta el proceso en microservicio

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,41 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { ConfigEnum } from './config';
+import { ConfigService } from '@nestjs/config';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+
+const validationPipe = new ValidationPipe({
+  whitelist: true,
+  forbidNonWhitelisted: true,
+});
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const logger = new Logger(AppModule.name);
+
+  const context = await NestFactory.createApplicationContext(AppModule);
+
+  const config = context.get(ConfigService);
+
+  const PORT = config.get<number>(ConfigEnum.PORT, { infer: true });
+  const HOST = config.get<number>(ConfigEnum.HOST, { infer: true });
+
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port: PORT,
+        host: HOST,
+      },
+    },
+  );
+
+  app.useGlobalPipes(validationPipe);
+
+  await app.listen();
+
+  logger.log(`Orders Microservice running on port ${PORT}`);
 }
 
 bootstrap();
